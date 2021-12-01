e661e0a700e5db4344bcc3cc376bd385ad26c9d5
Alessio Matricardi <alessiomatricardi@yahoo.com>
2021-12-01 23:31:14 +0100

    spacy is needed

--- test.sh
+++ test.sh
@@ -5,7 +5,7 @@ echo 'Actual db will be renamed to old_messages_ms.db'
 echo 'Remember to rename it if you want to use'
 mv -f messages_ms.db old_messages_ms.db || echo 'messages_ms.db not exists... continue with tests'
 
-pytest -s --cov mib
+pytest -s --cov-report term-missing --cov mib
 
 mv -f messages_ms.db messages_ms_test.db
 ( mv -f old_messages_ms.db messages_ms.db && rm -f messages_ms_test.db ) || ( echo 'old_messages_ms.db not exists... messages_ms.db from test will be held' && mv -f messages_ms_test.db messages_ms.db )
