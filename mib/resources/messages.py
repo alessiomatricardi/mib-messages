79482c2353297554e0f2bd1565767a3aa4102db1
Alessio Matricardi <alessiomatricardi@yahoo.com>
2021-12-07 01:41:31 +0100

    implement image upload and modify draft

--- mib/resources/messages.py
+++ mib/resources/messages.py
@@ -34,7 +34,7 @@ def new_message():
     content = data.get('content')
     deliver_time = data.get('deliver_time')
     recipients = data.get('recipients')
-    img_base64 = data.get('image')
+    image_base64 = data.get('image')
     image_filename = data.get('image_filename')
     is_draft = data.get('is_draft')
 
@@ -126,7 +126,7 @@ def new_message():
     message.sender_id = requester_id
     message.content = content
     message.deliver_time = datetime.datetime.fromisoformat(deliver_time)
-    message.image = img_base64  #TODO VALUTARE SE FARLO DIVENTARE BOOLEANO
+    message.image = image_filename
     if not is_draft:
         # save message as pending
         message.is_sent = True
@@ -139,29 +139,27 @@ def new_message():
         message_recipient = Message_Recipient()
 
         message_recipient.id = message.id
-        message_recipient.is_read = False               # redundant because the db automatically set it to False
+        message_recipient.is_read = False
         message_recipient.recipient_id = recipient_id
 
         MessageManager.create_message_recipient(message_recipient)
 
-    # TODO IMPORTANTE!!!!!!!!!
-    # CARTELLA ATTACHMENTS GIA' CREATA IN __INIT__.PY
+    # save attachment
+    if image_base64 is not '':
+        basepath = os.path.join(os.getcwd(), 'mib', 'static', 'attachments')
 
-    if img_base64 is not '':
         # create a subdirectory of 'attachments' having as name the id of the message
-        os.mkdir(os.path.join(os.getcwd(), 'mib', 'static', 'attachments', str(id)))
-
-        # save attachment
+        os.mkdir(os.path.join(basepath, str(message.id)))
         
         # decoding image
-        img_data = BytesIO(base64.b64decode(img_base64))
+        image_data = BytesIO(base64.b64decode(image_base64))
 
         # store it, in case of a previously inserted image it's going to be overwritten
         try:
 
-            img = Image.open(img_data)
-            path_to_save = os.path.join(os.getcwd(), 'mib', 'static', 'attachments',
-                                    str(id), image_filename)
+            img = Image.open(image_data)
+            path_to_save = os.path.join(basepath,
+                                    str(message.id), image_filename)
             
             img.save(path_to_save, quality=100, subsampling=0)
 
@@ -825,8 +823,8 @@ def modify_draft_message(message_id):
     content = data.get('content')
     deliver_time = data.get('deliver_time')
     recipients = data.get('recipients')
-    new_img_base64 = data.get('new_image')
-    new_image_filename = data.get('new_image_filename')
+    image_base64 = data.get('image')
+    image_filename = data.get('image_filename')
     # TODO SUPPORT IT
     delete_image = data.get('delete_image')
     is_sent = data.get('is_sent')
@@ -851,17 +849,106 @@ def modify_draft_message(message_id):
 
     draft_message = MessageManager.retrieve_message_by_id(DRAFT_LABEL, message_id)
 
+    draft_message.content = content
+    draft_message.deliver_time = datetime.datetime.fromisoformat(deliver_time)
 
-    # TODO TODO TODO GUARDA NEW MESSAGE COME E' STRUTTURATO
+    # retrieve user blackist
+    blacklist = []
 
-    # TODO SUPPORT IMAGE DELETION
+    try:
+        data = {'requester_id': requester_id}
+        response = requests.get("%s/blacklist" % (BLACKLIST_ENDPOINT),
+                                    timeout=REQUESTS_TIMEOUT_SECONDS,
+                                    json=data)
+        if response.status_code != 200:
+
+            return response.json(), response.status_code
+        
+        blacklist = response.json()['blacklist']
+
+    except (requests.exceptions.ConnectionError, requests.exceptions.Timeout):
+        response_object = {
+            'status': 'failure',
+            'description': 'Error in retrieving blacklist',
+        }
+        return jsonify(response_object), 500
+
+    # used to store the list of valid recipients
+    valid_recipient_ids = []
+
+    # retrieve actual recipients
+    actual_recipient_ids = MessageManager.retrieve_message_recipients(draft_message.id)
+
+    # checks on recipients
+    for recipient_email in recipients:
+
+        # checking recipient availability
+        try:
+
+            # checking that the recipient's email corresponds to an existing user
+            response = requests.get("%s/users/%s" % (USERS_ENDPOINT, str(recipient_email)),
+                                    timeout=REQUESTS_TIMEOUT_SECONDS)
+
+            if response.status_code != 200:
+
+                return response.json(), response.status_code
+
+            # retrieve user data
+            recipient_json = response.json()['user']
+            recipient = User.build_from_json(recipient_json)
+
+            # checking that the retrieved user corresponds to an available one
+            # if not, skip him
+            if not recipient.is_active or recipient.id in blacklist:
+                continue
+            
+            # the current recipient user passed all checks
+            valid_recipient_ids.append(recipient.id)
+
+        except (requests.exceptions.ConnectionError, requests.exceptions.Timeout):
+            response_object = {
+                'status': 'failure',
+                'description': 'Error in retrieving user',
+            }
+            return jsonify(response_object), 500
+    
+    # if the message has no recipients, return with an error
+    if len(valid_recipient_ids) == 0:
+        response_object = {
+            'status' : 'failure',
+            'description' : 'Message has no valid recipients'
+        }
+        return jsonify(response_object), 400
+
+    for recipient_id in valid_recipient_ids:
+        # add to db only those who aren't already present
+        if recipient_id not in actual_recipient_ids:
+            # creating message recipient instance
+            message_recipient = Message_Recipient()
+
+            message_recipient.id = draft_message.id
+            message_recipient.is_read = False
+            message_recipient.recipient_id = recipient_id
+
+            MessageManager.create_message_recipient(message_recipient)
+
+        else:
+            # remove it from actual recipients
+            actual_recipient_ids.remove(recipient_id)
+    
+    # remaining ids should be removed because they are no more 'valid'
+    for recipient_id in actual_recipient_ids:
+        MessageManager.remove_message_recipient(draft_message.id, recipient.id)
+
+
+
     '''
     An user can decide to delete current attachment OR to replace it with another one
     They are mutual exclusive but, in case of wrong configuration made
     by the Gateway (delete=True and image provided), image deletion has the precedence
-    Note that if delete_image=False and new_image = '' -> keep current attachment
+    Note that if delete_image=False and image = '' -> keep current attachment
     '''
-    basepath = os.path.join(os.getcwd(), 'mib', 'static', 'attachments', str(id))
+    basepath = os.path.join(os.getcwd(), 'mib', 'static', 'attachments', str(draft_message.id))
 
     # remove attachment, if present
     if delete_image:
@@ -872,11 +959,14 @@ def modify_draft_message(message_id):
 
             # remove folder
             os.rmdir(basepath)
-        pass
-    elif new_img_base64 != '' and new_image_filename != '':
+        
+        # reset image field inside db
+        draft_message.image = ''
+
+    elif image_base64 != '' and image_filename != '':
         # replace old attachment (if present) with the new one
 
-        
+        new_filename = None
         
         # if not exists, create a subdirectory of 'attachments' having as name the id of the message
         if not os.path.exists(basepath):
@@ -890,19 +980,22 @@ def modify_draft_message(message_id):
                 if os.path.isfile(old_filename):
                     os.rename(old_filename, new_filename)
 
-        # save attachment
+        # save the new attachment
         
         # decoding image
-        img_data = BytesIO(base64.b64decode(new_img_base64))
+        image_data = BytesIO(base64.b64decode(image_base64))
 
         # store it, in case of a previously inserted image with the same name
         # it's going to be overwritten
         try:
 
-            img = Image.open(img_data)
-            path_to_save = os.path.join(path, new_image_filename)
+            img = Image.open(image_data)
+            path_to_save = os.path.join(basepath, image_filename)
             
             img.save(path_to_save, quality=100, subsampling=0)
+
+            # save inside db new attachment filename
+            draft_message.image = image_filename
 
         except Exception:
             response_object = {
@@ -911,12 +1004,23 @@ def modify_draft_message(message_id):
             }
             return jsonify(response_object), 500
 
+        # delete old attachment, if present
+        if new_filename is not None:
+            os.remove(new_filename)
+
     
     # set the message as sent
     # The message is in 'pending' status by now
     if is_sent:
         draft_message.is_sent = True
-        MessageManager.update_message(draft_message)
+    
+    MessageManager.update_message(draft_message)
+    
+    response_object = {
+        'status': 'success',
+        'description': 'Draft message saved' + ' and sent' if is_sent else '',
+    }
+    return jsonify(response_object), 200
 
 
 # DELETE OPERATIONS
