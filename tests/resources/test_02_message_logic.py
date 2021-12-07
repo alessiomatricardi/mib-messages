078cdc35477848d5498c7aee3d44b4658c15eeb1
m4r-kus <parti.marco@gmail.com>
2021-12-07 11:41:08 +0100

    tests on message logic
    Co-authored-by: RiccardoBonini <RiccardoBonini@users.noreply.github.com>

--- tests/resources/test_02_message_logic.py
+++ tests/resources/test_02_message_logic.py
@@ -4,14 +4,66 @@ import responses
 #from mib import db
 from flask_sqlalchemy import SQLAlchemy
 import datetime
+from mib.logic.user import User
 
 tested_app = create_app()
+
+USERS_ENDPOINT = tested_app.config['USERS_MS_URL']
+BLACKLIST_ENDPOINT = tested_app.config['BLACKLIST_MS_URL']
 
 
 class LogicTest(unittest.TestCase):
     from mib.logic.message_logic import MessageLogic
 
     msg_logic = MessageLogic()
+
+    sender_json = {
+            'id':10, 
+            'email':'prova10@mail.com', 
+            'is_active':True, 
+            'firstname':'Decimo',
+            'lastname':'Meridio',
+            'date_of_birth':'1996-06-06',
+            'lottery_points':0,
+            'has_picture':False,
+            'content_filter_enabled':False
+        }
+
+    sender_json_content_filter = {
+            'id':10, 
+            'email':'prova10@mail.com', 
+            'is_active':True, 
+            'firstname':'Decimo',
+            'lastname':'Meridio',
+            'date_of_birth':'1996-06-06',
+            'lottery_points':0,
+            'has_picture':False,
+            'content_filter_enabled':True
+        }
+
+    recipient_json = {
+        'id':11, 
+        'email':'prova11@mail.com', 
+        'is_active':True, 
+        'firstname':'Undici',
+        'lastname':'Leoni',
+        'date_of_birth':'1926-03-21',
+        'lottery_points':0,
+        'has_picture':False,
+        'content_filter_enabled':False
+    }
+
+    recipient_json_content_filter = {
+        'id':11, 
+        'email':'prova11@mail.com', 
+        'is_active':True, 
+        'firstname':'Undici',
+        'lastname':'Leoni',
+        'date_of_birth':'1926-03-21',
+        'lottery_points':0,
+        'has_picture':False,
+        'content_filter_enabled':True
+    }
 
     test_message_id = 4
     test_draft_id = 5
@@ -64,26 +116,244 @@ class LogicTest(unittest.TestCase):
 
             self.test_draft_id = message.id
 
-
+    @responses.activate
     def test_01_get_received_message(self):
+        from mib.models.message import Message
+        from mib.models.message import Message_Recipient
+        from mib.dao.message_manager import MessageManager
 
-        # 404 message doesn't exist
-        # 403 not recipient
-        # 404 message is hide
-        # 500 user is not activa
-        # mocking
-        # send email in reading first time
-        # 500 blacklist is not available
-        # success with content filter enable
-        # success with content filter disable
+        with tested_app.app_context():
 
-        pass
+            received_message = MessageManager.retrieve_message_by_id('received',self.test_message_id)
+            non_existing_message = MessageManager.retrieve_message_by_id('received',1000)
 
+            self.assertEqual(received_message.sender_id,self.sender_json['id'])
+
+            requester = User.build_from_json(self.recipient_json)
+            wrong_requester =  User.build_from_json(self.sender_json)
+            requester_content_filter = User.build_from_json(self.recipient_json_content_filter)
+
+            # 404 message doesn't exist
+            result, code = self.msg_logic.get_received_message(non_existing_message,requester)
+            self.assertEqual(code,404)
+
+            # 403 not recipient
+            result, code = self.msg_logic.get_received_message(received_message,wrong_requester)
+            self.assertEqual(code,403)
+            
+            # 500 user ma is not active
+            result, code = self.msg_logic.get_received_message(received_message,requester)
+            self.assertEqual(code,500)
+
+            # mocking users
+            responses.add(responses.GET, "%s/users/%s" % (USERS_ENDPOINT, str(10)),
+                    json={'status': 'Current user present','user':self.sender_json}, status=404)
+
+            responses.add(responses.GET, "%s/users/%s" % (USERS_ENDPOINT, str(11)),
+                    json={'status': 'Current user present','user':self.recipient_json}, status=200)
+
+            # failure 404 when sender is not found
+            result, code = self.msg_logic.get_received_message(received_message,requester)
+            self.assertEqual(code,404)
+            
+            # now the sender exists
+            responses.replace(responses.GET, "%s/users/%s" % (USERS_ENDPOINT, str(10)),
+                    json={'status': 'Current user present','user':self.sender_json}, status=200)
+
+            
+            # send email in reading first time but 500 blacklist is not available
+            result, code = self.msg_logic.get_received_message(received_message,requester)
+            self.assertEqual(code,500)
+            
+            # mocking blacklist but it returns 500
+            responses.add(responses.GET, "%s/blacklist" % (BLACKLIST_ENDPOINT),
+                    json={'status': 'Current user present','blacklist':[]}, status=500)
+
+            # failure 500 when blacklist returns an Internal Server Error
+            result, code = self.msg_logic.get_received_message(received_message,requester)
+            self.assertEqual(code,500)
+            
+            # now the blacklist microservice returns 200 
+            responses.replace(responses.GET, "%s/blacklist" % (BLACKLIST_ENDPOINT),
+                    json={'status': 'Current user present','blacklist':[]}, status=200)
+            
+            # success with content filter disable
+            result, code = self.msg_logic.get_received_message(received_message,requester)
+            self.assertEqual(code,200)
+            
+            # enabling content filter
+            responses.replace(responses.GET, "%s/users/%s" % (USERS_ENDPOINT, str(11)),
+                    json={'status': 'Current user present','user':self.recipient_json_content_filter}, status=200)
+
+            # success with content filter enable
+            result, code = self.msg_logic.get_received_message(received_message,requester_content_filter)
+            self.assertEqual(code,200)
+
+            # 404 message is hide
+            message_recipient = MessageManager.retrieve_message_recipient(received_message.id,requester.id)
+            MessageManager.hide_message(message_recipient)
+            result, code = self.msg_logic.get_received_message(received_message,requester)
+            self.assertEqual(code,404)
+
+    @responses.activate
     def test_02_get_draft_message(self):
-        pass
+        from mib.dao.message_manager import MessageManager
 
+        with tested_app.app_context():
+
+            draft_message = MessageManager.retrieve_message_by_id('drafts',self.test_draft_id)
+            non_existing_message = MessageManager.retrieve_message_by_id('drafts',1000)
+
+            self.assertEqual(draft_message.sender_id,self.sender_json['id'])
+
+            requester = User.build_from_json(self.sender_json)
+            wrong_requester =  User.build_from_json(self.recipient_json)
+
+            # message not found
+            result, code = self.msg_logic.get_draft_message(non_existing_message,requester)
+            self.assertEqual(code,404)
+
+            # not the sender 403
+            result, code = self.msg_logic.get_draft_message(draft_message,wrong_requester)
+            self.assertEqual(code,403)
+
+            # 500 blacklist not active
+            result, code = self.msg_logic.get_draft_message(draft_message,requester)
+            self.assertEqual(code,500)
+
+            # mocking blacklist
+            responses.add(responses.GET, "%s/blacklist" % (BLACKLIST_ENDPOINT),
+                    json={'status': 'Current user present','blacklist':[]}, status=500)
+
+            # 500 when blacklist returns an Internal Server Error
+            result, code = self.msg_logic.get_draft_message(draft_message,requester)
+            self.assertEqual(code,500)
+
+            # mocking blacklist
+            responses.replace(responses.GET, "%s/blacklist" % (BLACKLIST_ENDPOINT),
+                    json={'status': 'Current user present','blacklist':[]}, status=200)
+
+            # 500 users not active while checking that the recipients previously selected exist
+            result, code = self.msg_logic.get_draft_message(draft_message,requester)
+            self.assertEqual(code,500)
+
+            # mocking users but it returns 404
+            responses.add(responses.GET, "%s/users/%s" % (USERS_ENDPOINT, str(10)),
+                    json={'status': 'Current user present','user':self.sender_json}, status=200)
+
+            responses.add(responses.GET, "%s/users/%s" % (USERS_ENDPOINT, str(11)),
+                    json={'status': 'Current user present','user':self.recipient_json}, status=404)
+
+            # fail 404 when recipient not found
+            result, code = self.msg_logic.get_draft_message(draft_message,requester)
+            self.assertEqual(code,404)
+
+            # now recipient users returns 200
+            responses.replace(responses.GET, "%s/users/%s" % (USERS_ENDPOINT, str(11)),
+                    json={'status': 'Current user present','user':self.recipient_json}, status=200)
+
+            # previous recipients are either in the blacklist or not active
+            responses.replace(responses.GET, "%s/blacklist" % (BLACKLIST_ENDPOINT),
+                    json={'status': 'Current user present','blacklist':[11]}, status=200)
+
+            # 200 success
+            result, code = self.msg_logic.get_draft_message(draft_message,requester)
+            self.assertEqual(code,200)
+
+
+    @responses.activate
     def test_03_get_pending_or_delivered_message(self):
-        pass
+        from mib.dao.message_manager import MessageManager
+
+        with tested_app.app_context():
+
+            delivered_message = MessageManager.retrieve_message_by_id('delivered',self.test_message_id)
+            non_existing_message = MessageManager.retrieve_message_by_id('delivered',1000)
+
+            self.assertEqual(delivered_message.sender_id,self.sender_json['id'])
+
+            requester = User.build_from_json(self.sender_json)
+            requester_content_filter = User.build_from_json(self.sender_json_content_filter)
+            wrong_requester =  User.build_from_json(self.recipient_json)
+
+            # message not found
+            result, code = self.msg_logic.get_pending_or_delivered_message(non_existing_message,requester)
+            self.assertEqual(code,404)
+
+            # not the sender 403
+            result, code = self.msg_logic.get_pending_or_delivered_message(delivered_message,wrong_requester)
+            self.assertEqual(code,403)
+
+            # 500 blacklist not active
+            result, code = self.msg_logic.get_pending_or_delivered_message(delivered_message,requester)
+            self.assertEqual(code,500)
+
+            # mocking blacklist
+            responses.add(responses.GET, "%s/blacklist" % (BLACKLIST_ENDPOINT),
+                    json={'status': 'Current user present','blacklist':[]}, status=500)
+
+            # 500 when blacklist returns an Internal Server Error
+            result, code = self.msg_logic.get_pending_or_delivered_message(delivered_message,requester)
+            self.assertEqual(code,500)
+
+            # mocking blacklist
+            responses.replace(responses.GET, "%s/blacklist" % (BLACKLIST_ENDPOINT),
+                    json={'status': 'Current user present','blacklist':[]}, status=200)
+
+            # 500 users not active while checking that the recipients previously selected exist
+            result, code = self.msg_logic.get_pending_or_delivered_message(delivered_message,requester)
+            self.assertEqual(code,500)
+
+            # mocking users but it returns 404
+            responses.add(responses.GET, "%s/users/%s" % (USERS_ENDPOINT, str(10)),
+                    json={'status': 'Current user present','user':self.sender_json}, status=200)
+
+            responses.add(responses.GET, "%s/users/%s" % (USERS_ENDPOINT, str(11)),
+                    json={'status': 'Current user present','user':self.recipient_json}, status=404)
+
+            # fail 404 when recipient not found
+            result, code = self.msg_logic.get_pending_or_delivered_message(delivered_message,requester)
+            self.assertEqual(code,404)
+
+            # now recipient users returns 200
+            responses.replace(responses.GET, "%s/users/%s" % (USERS_ENDPOINT, str(11)),
+                    json={'status': 'Current user present','user':self.recipient_json}, status=200)
+
+            # previous recipients are either in the blacklist or not active
+            responses.replace(responses.GET, "%s/blacklist" % (BLACKLIST_ENDPOINT),
+                    json={'status': 'Current user present','blacklist':[11]}, status=200)
+
+            # now the sender has content filter enabled 
+            responses.replace(responses.GET, "%s/users/%s" % (USERS_ENDPOINT, str(10)),
+                    json={'status': 'Current user present','user':self.sender_json_content_filter}, status=200)
+
+            # 200 success
+            result, code = self.msg_logic.get_pending_or_delivered_message(delivered_message,requester_content_filter)
+            self.assertEqual(code,200)
+
     
+    @responses.activate
     def test_04_delete_message(self):
-        pass
+        from mib.dao.message_manager import MessageManager
+
+        with tested_app.app_context():
+
+            draft_message = MessageManager.retrieve_message_by_id('drafts',self.test_draft_id)
+            non_existing_message = MessageManager.retrieve_message_by_id('drafts',1000)
+
+            self.assertEqual(draft_message.sender_id,self.sender_json['id'])
+
+            requester = User.build_from_json(self.sender_json)
+            wrong_requester =  User.build_from_json(self.recipient_json)
+
+            # message not found
+            result, code = self.msg_logic.delete_message(non_existing_message,requester)
+            self.assertEqual(code,404)
+
+            # 403 sender not corresponing
+            result, code = self.msg_logic.delete_message(draft_message,wrong_requester)
+            self.assertEqual(code,403)
+
+            # 200 success
+            result, code = self.msg_logic.delete_message(draft_message,requester)
+            self.assertEqual(code,403)
